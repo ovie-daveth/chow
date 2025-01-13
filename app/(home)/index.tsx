22dfea25443b7e906c310f54416ed93f4fc1254d
ovie-daveth <oviedavid77@gmail.com>
2025-01-13 21:55:14 +0000

    first personal commit for 2025

---
+++ app/(home)/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View className=" text-red-500 bg-black h-screen">
+      <Text className=" text-red-500 mt-20" >Hello world</Text>
+    </View>
+  )
+}
